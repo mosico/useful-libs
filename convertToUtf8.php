0d533f47852898f12514d5f1c69d29ca2c1e1c41
mosico <qiwei.mo@gmail.com>
2014-11-28 14:22:48 +0800

    upgrade convert, include object.

--- convertToUtf8.php
+++ convertToUtf8.php
@@ -1,24 +1,55 @@
 <?php
 
-function convertToUtf8($array) {
-    $converted = array();
-    foreach($array as $key => $value) {
-        if(is_array($value)) {
-            $utf8Val = $this->convertToUtf8($value);
+class Common
+{
+    public static function convertToUtf8($var)
+    {
+        $isObject = is_object($var);
+        $isArray = is_array($var);
+        if ($isObject) {
+            $className = get_class($var);
+            $converted = new $className();
+        } elseif ($isArray) {
+            $converted = array();
+        } elseif (is_string($var)) {
+            $converted = self::_convertCore($var);
         } else {
-            if (function_exists('mb_detect_encoding')) {
-                $encoding = mb_detect_encoding($value, 'auto', true);
-                if ($encoding == 'UTF-8') {
-                    $utf8Val = $value;
+            $converted = $var;
+        }
+        if ($isObject || $isArray) {
+            foreach ($var as $key => $value) {
+                if (is_object($value) || is_array($value)) {
+                    $utf8Val = self::convertToUtf8($value);
                 } else {
-                    $encoding = $encoding ?: 'auto';
-                    $utf8Val = mb_convert_encoding($value, "UTF-8", $encoding);
+                    $utf8Val = self::_convertCore($value);
                 }
-            } else {
-                $utf8Val = iconv(false, "UTF-8//IGNORE", $value);
+                if ($isObject) {
+                    $converted->$key = $utf8Val;
+                } elseif ($isArray) {
+                    $converted[$key] = $utf8Val;
+                } else {
+                    $converted = $utf8Val;
+                }
             }
         }
-        $converted[$key] = $utf8Val;
+        return $converted;
     }
-    return $converted;
+
+    private static function _convertCore($string)
+    {
+        $utf8Val = '';
+        if (function_exists('mb_detect_encoding')) {
+            $encoding = mb_detect_encoding($string, 'auto', true);
+            if ($encoding == 'UTF-8') {
+                $utf8Val = $string;
+            } else {
+                $encoding = $encoding ?: 'auto';
+                $utf8Val = mb_convert_encoding($string, "UTF-8", $encoding);
+            }
+        } else {
+            $utf8Val = iconv(false, "UTF-8//IGNORE", $string);
+        }
+        return $utf8Val;
+    }
+
 }
